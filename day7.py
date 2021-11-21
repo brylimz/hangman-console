5332d5f5eab0e8f43530b6e3fd4a771ab2e49f9d
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-21 11:11:13 +0700

    while added

--- day7.py
+++ day7.py
@@ -1,11 +1,23 @@
 import random
+
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(f"psst the solution is {chosen_word}")
-guess = input("Guess a letter:").lower()
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length):
+    display += "_"
+print(display)
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter:").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
