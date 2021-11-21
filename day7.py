348133d56a8f46516ddbbca7627c21f9dbb6bdde
Bryan`Y. Limz <superuser@brylimz.com>
2021-11-21 12:06:29 +0700

    finish

--- day7.py
+++ day7.py
@@ -1,23 +1,42 @@
 import random
+import hangman_words
+import hangman_art
 
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
-print(f"psst the solution is {chosen_word}")
-display = []
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
-for _ in range(word_length):
-    display += "_"
-print(display)
 
 end_of_game = False
-while not end_of_game:
-    guess = input("Guess a letter:").lower()
+lives = 6
 
+print(hangman_art.logo)
+print(f"Pssst, the solution is {chosen_word}.")
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
     for position in range(word_length):
         letter = chosen_word[position]
+
         if letter == guess:
             display[position] = letter
-    print(display)
+
+    if guess not in chosen_word:
+        print(F"you guessed {guess}, that's not in the word. You lose a life. ")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    print(f"{' '.join(display)}")
+
     if "_" not in display:
         end_of_game = True
-        print("You Win")
+        print("You win.")
+
+    print(hangman_art.stages[lives])
